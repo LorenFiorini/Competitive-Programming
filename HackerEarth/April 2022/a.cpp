7f28e8ea4338073e67d02643c4606f302bf1aad6
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2022-08-15 17:26:51 +0200

    HackerEarth

---
+++ HackerEarth/April 2022/a.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF (int)1e9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+//
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<string> vstr;
+typedef vector<pii> vpii;
+typedef vector<vint> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+#define mk make_pair
+#define pb push_back
+//
+#define por(i, a, b, in) for (int i=a ; i<(b) ; i+=in)
+#define rep(i, a, b) por(i, a, b, 1)
+#define each(it, mp) for (auto it = mp.begin(); it != mp.end(); it++)
+#define all(c) c.begin(), c.end()
+#define allg(c) all(c), greater<> ()
+#define len(v) (int) v.size()
+//
+#define show(x) cout << x << "\n";
+#define showp(x, y) cout << x << " " << y << "\n";
+#define showv(v, n) rep(i, 0, n) {cout << v[i] << " ";} cout << "\n";
+#define showvp(v, n) rep(i, 0, n) {showp(v[i].ff, v[i].ss);}
+#define showvv(vv, n, m) rep(r, 0, n) {showv(vv[r], m);}
+#define showset(it, st) each(it, st) {cout << *it << " ";} cout << "\n";
+#define showmap(it, mp) each(it, mp) {cout << it->ff << " " << it->ss << "\n";} cout << "\n";
+#define precision(d) cout << setprecision(d) << fixed;
+#define ff first
+#define ss second
+
+ll mod = 1e9 + 7;
+
+void next(ll num[3]) {
+	swap(num[0], num[1]);
+	swap(num[1], num[2]);
+	num[2] += num[0] + num[1];
+	num[2] %= mod;
+}
+
+void solve () {	
+	ll ans = 14;
+	ll n;
+	cin >> n;
+	if (n <= 3) {
+		if (n==1) ans = 9;
+		if (n==2) ans = 13;
+		show(ans);
+		return;
+	}
+	
+	ll num[3] = {3,2,1};
+	rep(i, 3, n) {
+		next(num);
+		ans += (num[2] * num[2]) % mod;
+		//showp(i+1, ans);		
+		ans %= mod;
+	}
+	
+	
+	show(ans);
+}
+
+int main () 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	solve();
+	
+	return 0;
+}
+
+// Lorenzo Fiorini
