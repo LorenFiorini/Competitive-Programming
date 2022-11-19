d859ebc9b9856dc89d264e44ab8b75aec9dc8aab
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2022-11-19 06:19:46 +0100

    ABC276 @ 42

---
+++ atCoder/ABC276/b.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+#define INF (int)1e9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+//
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<string> vstr;
+typedef vector<pii> vpii;
+typedef vector<vint> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+#define mk make_pair
+#define pb push_back
+//
+#define por(i, a, b, in) for (int i=a ; i<(b) ; i+=in)
+#define rep(i, a, b) por(i, a, b, 1)
+#define each(it, mp) for (auto it = mp.begin(); it != mp.end(); it++)
+#define all(c) c.begin(), c.end()
+#define allg(c) all(c), greater<> ()
+#define len(v) (int) v.size()
+//
+#define show(x) cout << x << "\n";
+#define showp(x, y) cout << x << " " << y << "\n";
+#define showv(v, n) rep(i, 0, n) {cout << v[i] << " ";} cout << "\n";
+#define showvp(v, n) rep(i, 0, n) {showp(v[i].ff, v[i].ss);}
+#define showvv(vv, n, m) rep(r, 0, n) {showv(vv[r], m);}
+#define showset(it, st) each(it, st) {cout << *it << " ";} cout << "\n";
+#define showmap(it, mp) each(it, mp) {cout << it->ff << " " <ls < it->ss << "\n";} cout << "\n";
+#define precision(d) cout << setprecision(d) << fixed;
+#define ff first
+#define ss second
+
+void solve () {
+	int n, m;
+	cin >> n >> m;
+	vvi v(n);
+	int a,b;
+	
+	rep (i, 0, m) {
+	    cin >> a >> b;
+	    a--; b--;
+	    v[a].pb(b);
+	    v[b].pb(a);
+	}
+	
+	rep (i, 0, n) {
+	    a = len(v[i]);
+	    cout << a << " ";
+	    sort(all(v[i]));
+	    rep(j, 0, a) cout << (v[i][j] + 1) << " ";
+	    cout << endl;
+	}
+	
+}
+ 
+int main () 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	solve();
+	
+	return 0;
+}
+ 
+// Lorenzo Fiorini
