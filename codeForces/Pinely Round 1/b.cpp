05cb72cda9d60aaec57fde0f266884dd6d31882b
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-01-15 14:54:13 +0100

    CodeForces CodeTON & Pinely

---
+++ codeForces/Pinely Round 1/b.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF (int)1e9
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+//
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<string> vstr;
+typedef vector<pii> vpii;
+typedef vector<vint> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+#define mk make_pair
+#define pb push_back
+//
+#define por(i, a, b, in) for (int i=a ; i<(b) ; i+=in)
+#define rep(i, a, b) por(i, a, b, 1)
+#define each(it, mp) for (auto it = mp.begin(); it != mp.end(); it++)
+#define all(c) c.begin(), c.end()
+#define allg(c) all(c), greater<> ()
+#define len(v) (int) v.size()
+//
+#define show(x) cout << x << "\n";
+#define showp(x, y) cout << x << " " << y << "\n";
+#define showv(v, n) rep(i, 0, n) {cout << v[i] << " ";} cout << "\n";
+#define showvp(v, n) rep(i, 0, n) {showp(v[i].ff, v[i].ss);}
+#define showvv(vv, n, m) rep(r, 0, n) {showv(vv[r], m);}
+#define showset(it, st) each(it, st) {cout << *it << " ";} cout << "\n";
+#define showmap(it, mp) each(it, mp) {cout << it->ff << " " << it->ss << "\n";} cout << "\n";
+#define precision(d) cout << setprecision(d) << fixed;
+#define ff first
+#define ss second
+
+map<vint, int> mp;
+bool cool(vint &v) {
+	seti st(all(v));
+	return len(st) == len(v);
+}
+
+int fun(vint v, int d, int n) {
+	if (n <= 3) return n;
+	if (d >= 0)	v.erase(v.begin() + d);
+
+	if (mp.find(v) != mp.end()) return mp[v];
+	
+	int ans = 0;
+	if (cool(v)) {
+		ans = n;
+	} else {
+		int pre, nxt, id;
+		rep (i, 1, n+1) {
+			pre = (i-1) % n;
+			nxt = (i+1) % n;
+			id = i % n;
+			if (v[pre] != v[nxt]) {
+				ans = max(fun(v, id, n-1), ans);
+			}
+		}
+	}
+	if (ans == 0) ans = (n / 2);
+	
+	mp[v] = ans + 1;
+	return mp[v];
+}
+
+void solve () {
+	int ans = 0;
+	int n;
+	cin >> n;
+	vint v(n);
+	rep (i, 0, n) cin >> v[i];
+
+	ans = fun(v, -1, n);
+
+	show(ans);
+}
+
+int main () 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int t; cin >> t; while (t--)
+	
+	solve();
+	
+	return 0;
+}
+
+// Lorenzo Fiorini
