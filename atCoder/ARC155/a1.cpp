bb7acde11c4df34c14e83ce2607ef774a5138206
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-02-02 16:49:45 +0100

    ARC155 & AGC59

---
+++ atCoder/ARC155/a1.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+// My Favorite Macros
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<string> vstr;
+typedef vector<pii> vpii;
+typedef vector<vint> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+#define mk make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define por(i, a, b, in) for (ll i = a; i < (b); i += in)
+#define rep(i, a, b) por(i, a, b, 1)
+#define each(it, mp) for (auto it = mp.begin(); it != mp.end(); it++)
+#define all(c) c.begin(), c.end()
+#define allg(c) all(c), greater<> ()
+#define len(v) (int) v.size()
+#define precision(d) cout << setprecision(d) << fixed;
+// Debugging Tools
+#define show(x) cout << x << "\n";
+#define showbin(x) for (int i = (1 << 30); i > 0; i >>= 1) {cout << ((x&i)? 1: 0);} cout << "\n";
+#define showp(x, y) cout << x << " " << y << endl;
+#define showv(v) for (auto val: v) {cout << val << " ";} cout << "\n";
+#define showvp(v) for (auto par: v) {showp(par.ff, par.ss);}
+#define showmat(mat) for (auto row: mat) {showv(row);}
+#define showset(it, st) each(it, st) {cout << *it << " ";} cout << "\n";
+#define showmap(it, mp) each(it, mp) {cout << it->ff << " " << it->ss << "\n";} cout << "\n";
+
+
+bool palindrome(string s, int le, int ri) {
+	while (le < ri) {
+		if (s[le] != s[ri]) return 0;
+	}
+	return 1;
+}
+
+string solve () {
+	ll n, k;
+	cin >> n >> k;
+	string s;
+	cin >> s;
+	
+	stack<char> q;
+	q.push('.');
+	for (char c: s) {
+		if (q.top() == c) {
+			q.pop(); 
+		} else {
+			q.push(c);
+		}
+	}
+	if (q.top() == '.')
+	return "Yes";
+
+	return "No";
+}
+
+int main () 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int t; cin >> t; while (t--)
+	
+	cout << solve() << endl;
+	
+	return 0;
+}
+
+// Lorenzo Fiorini
