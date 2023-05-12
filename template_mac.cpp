b5e3cf01a8ea2e3fe61c54497780adfbac5c969c
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2023-05-12 12:12:02 +0200

    templates in mac

--- template_mac.cpp
+++ template_mac.cpp
@@ -38,22 +38,22 @@ typedef multiset<int> mseti;
 #define pb push_back
 #define ff first
 #define ss second
-#define por(i, a, b, in) for (int i = a; i < (b); i += in)
-#define rep(i, a, b) por(i, a, b, 1)
-#define each(it, mp) for (auto it = mp.begin(); it != mp.end(); it++)
-#define all(c) c.begin(), c.end()
-#define allg(c) all(c), greater<> ()
-#define len(v) (int) v.size()
+#define por(i_, a_, b_, in_) for (int i_ = a_; i < (b_); i_ += in_)
+#define rep(i_, a_, b_) por(i_, a_, b_, 1)
+#define each(it_, mp_) for (auto it_ = mp_.begin(); it_ != mp_.end(); it_++)
+#define all(c_) c_.begin(), c_.end()
+#define allg(c_) all(c_), greater<> ()
+#define len(v_) v.size()
 #define precision(d) cout << setprecision(d) << fixed;
 // Debugging Tools
-#define show(x) cout << x << "\n";
-#define showbin(x) for (int i = (1 << 30); i > 0; i >>= 1) {cout << ((x&i)? 1: 0);} cout << "\n";
-#define showp(x, y) cout << x << " " << y << "\n";
-#define showv(v) for (auto val: v) {cout << val << " ";} cout << "\n";
-#define showvp(v) for (auto par: v) {showp(par.ff, par.ss);}
-#define showmat(mat) for (auto row: mat) {showv(row);}
-#define showset(it, st) each(it, st) {cout << *it << " ";} cout << "\n";
-#define showmap(it, mp) each(it, mp) {cout << it->ff << " " << it->ss << "\n";} cout << "\n";
+#define show(x_) cout << x_ << "\n";
+#define showbin(x_, n_) for (int i_ = (1 << n_); i_ > 0; i_ >>= 1) {cout << ((x_&i_)? 1: 0);} cout << "\n";
+#define showp(x_, y_) cout << x_ << " " << y_ << "\n";
+#define showv(v_) for (auto a_: v_) {cout << a_ << " ";} cout << "\n";
+#define showvp(v_) for (auto p_: v_) {showp(p_.first, p_.second);}
+#define showmat(mat_) for (auto r_: mat_) {showv(r_);}
+#define showset(st_) for (auto a_: st_) {cout << a_ << " ";} cout << "\n";
+#define showmap(mp_) for (auto a_: mp_) {cout << a_->first << " " << a_->second << "\n";} cout << "\n";
 
 
 void solve () {
@@ -68,7 +68,7 @@ void solve () {
 int main () 
 {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(nullptr);
 	
 	int t; cin >> t; while (t--)
 	
