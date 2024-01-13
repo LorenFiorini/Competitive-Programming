7f75cc0e8d4f5d5e7b12aac5dbc39315bf50addd
LorenFiorini <64514862+LorenFiorini@users.noreply.github.com>
2024-01-13 18:04:37 +0100

    919

---
+++ CodeForces/919/b1.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+// My Favorite Macros
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<string> vstr;
+typedef vector<pii> vpii;
+typedef vector<vint> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+#define mk make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define por(i_, a_, b_, in_) for (long long i_ = a_; i_ < (b_); i_ += in_)
+#define rep(i_, a_, b_) por(i_, a_, b_, 1)
+#define each(it_, mp_) for (auto it_ = mp_.begin(); it_ != mp_.end(); it_++)
+#define all(c_) c_.begin(), c_.end()
+#define allg(c_) all(c_), greater<> ()
+#define len(v_) v_.size()
+#define precision(d_) cout << setprecision(d_) << fixed;
+// Debugging Tools
+#define show(x_) cout << x_ << "\n";
+#define shows(x_) cout << x_ << " ";
+#define showbin(x_, n_) for (ll i_ = (1 << n_); i_ > 0; i_ >>= 1) {cout << ((x_&i_)? 1: 0);} cout << "\n";
+#define showp(x_, y_) cout << x_ << " " << y_ << "\n";
+#define showv(v_) for (auto a_: v_) {cout << a_ << " ";} cout << "\n";
+#define showvp(v_) for (auto p_: v_) {showp(p_.first, p_.second);}
+#define showmat(mat_) for (auto r_: mat_) {showv(r_);}
+#define showset(st_) for (auto a_: st_) {cout << a_ << " ";} cout << "\n";
+#define showmap(mp_) for (auto a_: mp_) {cout << a_->first << " " << a_->second << "\n";} cout << "\n";
+
+
+void solve (void) {
+	ll ans = 0;
+	ll n, a, b;
+	cin >> n >> a >> b;
+	vll v(n);
+    rep (i, 0, n) cin >> v[i];
+	sort(allg(v));
+
+	vll pre(n + 1);
+    rep (i, 0, n) {
+        pre[i + 1] = pre[i] + v[i];
+    }
+    // showp(a,b);
+    // showv(v);
+    // showv(pre);
+
+    // rep (i, 0, a+b + 1) {
+    //     ans = max(ans, pre[n] - pre[i]);
+    // }
+    // ll A = 0, B = pre[n];
+
+    // rep (k, b, a+b + 1) {
+    //     ll bob = pre[n] - pre[min(k + b, n)];
+    //     ans = max(ans, bob);
+    // }
+
+    ll alice = 0;
+    rep (k, 0, a+1) {
+        ll sum = pre[n] - pre[k];
+        ll bob = sum;
+        rep (j, 0, b + 1) {
+            ll x = k + j;
+            if (x > n) break;
+            // mn = min(mn, pre[n] - pre[k + x])
+            ll tmp = sum - 2 * (pre[x] - pre[k]);
+            // showp("  bob, ", tmp);
+            bob = min(bob, tmp);
+        }
+        alice = max(alice, bob);
+            // showp("alice, ", alice);
+
+    }
+    ans = alice;
+	show(ans);
+}
+
+int main (void) 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int t; cin >> t; while (t--)
+	
+	solve();
+	
+	return (0);
+}
+
+/* Lorenzo Fiorini */
---
+++ CodeForces/919/temp.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+// My Favorite Macros
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vint;
+typedef vector<ll> vll;
+typedef vector<string> vstr;
+typedef vector<pii> vpii;
+typedef vector<vint> vvi;
+typedef map<int,int> mpii;
+typedef set<int> seti;
+typedef multiset<int> mseti;
+#define mk make_pair
+#define pb push_back
+#define ff first
+#define ss second
+#define por(i_, a_, b_, in_) for (auto i_ = a_; i_ < b_; i_ += in_)
+#define rep(i_, a_, b_) por(i_, a_, b_, 1)
+#define each(it_, c_) for (auto it_ = c_.begin(); it_ != c_.end(); it_++)
+#define all(c_) c_.begin(), c_.end()
+#define allg(c_) all(c_), greater<> ()
+#define len(c_) c_.size()
+#define precision(d_) cout << setprecision(d_) << fixed;
+// Debugging Tools
+#define show(x_) cout << x_ << "\n";
+#define shows(x_) cout << x_ << " ";
+#define showbin(x_, n_) for (auto shift_ = n_; shift_ >= 0; shift_ -= 1) {cout << (int) ((x_ >> shift_) % 2);} cout << "\n";
+#define showp(x_, y_) cout << x_ << " " << y_ << "\n";
+#define showv(c_) for (auto a_: c_) {cout << a_ << " ";} cout << "\n";
+#define showvp(c_) for (auto p_: c_) {showp(p_.first, p_.second);}
+#define showmat(mat_) for (auto r_: mat_) {showv(r_);}
+#define showmap(mp_) for (auto a_: mp_) {cout << a_->first << " " << a_->second << "\n";} cout << "\n";
+
+
+void solve(void) {
+	int ans = 0;
+	int n;
+	cin >> n;
+	showbin(n, 5);
+
+	show(ans);
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int t; cin >> t; while (t--)
+	
+	solve();
+	
+	return (0);
+}
+
+/* Lorenzo Fiorini */
